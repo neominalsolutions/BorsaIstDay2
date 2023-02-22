c5e7a28679f669c1911f4d066bc588190cda22e0
mert.alptekin <mert.alptekin@neomina.com>
2023-02-22 10:28:50 +0300

    Rxjs Subjects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { ReactiveProgramlamaComponent } from './rxjs/reactive-programlama/reactive-programlama.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { RxjsSubjectsComponent } from './rxjs-subjects/rxjs-subjects.component';
 // uygulama genelinde http işlemlerin yönetilmesini sağlayan angular library
 // httpClient service ile uygulama componentlerine service injection yaparak data hizmetini veriyoruz.
 @NgModule({
-  declarations: [AppComponent, ReactiveProgramlamaComponent],
+  declarations: [AppComponent, ReactiveProgramlamaComponent, RxjsSubjectsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
